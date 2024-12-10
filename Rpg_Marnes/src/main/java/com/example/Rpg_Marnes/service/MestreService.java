2b6f79a78bcd0b30e03857ba4853d1a3f69759b0
pdtezza <pdtezza@gmail.com>
2024-12-10 12:10:39 -0300

    fazendo atualaizacoes na classe Mestre

--- Rpg_Marnes/src/main/java/com/example/Rpg_Marnes/service/MestreService.java
+++ Rpg_Marnes/src/main/java/com/example/Rpg_Marnes/service/MestreService.java
@@ -5,11 +5,9 @@ import org.springframework.stereotype.Service;
 @Service
 public class MestreService {
 
-    // Método para realizar a rolagem de dados
+    
     public int rolarDados(int numLados, int qntDados, int bonus) {
         int resultado = bonus;
-
-        // Simulação da rolagem
         for (int i = 0; i < qntDados; i++) {
             resultado += (int) (Math.random() * numLados) + 1; // Gera números entre 1 e numLados
         }
